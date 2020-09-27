daf8beb9823bb9ca66d7f41a1649a8c53ca7c4d3
Ji Young Mun <Ji.Mun@outlook.com>
2020-09-27 16:35:45 -0700

    Add project files.

---
+++ Data/EmployeeOrientationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeOrientation.Models;
+
+namespace EmployeeOrientation.Data
+{
+    public class EmployeeOrientationContext : DbContext
+    {
+        public EmployeeOrientationContext(DbContextOptions<EmployeeOrientationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeOrientation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
